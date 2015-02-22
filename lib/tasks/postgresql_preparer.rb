874f9934ca0fee7c30edaee6dd104110bd949edc
William Montgomery <wmontg2@uic.edu>
2015-02-22 12:24:26 -0600

    Call exec_prepared on connection and not statement

--- lib/tasks/postgresql_preparer.rb
+++ lib/tasks/postgresql_preparer.rb
@@ -1,10 +1,11 @@
 class PostgresqlPreparer
   def initialize
+    @name = 'insert_person'
     @statement = prepared_statement
   end
 
   def execute(*args)
-    statement.execute(args)
+    connection.exec_prepared(args)
   end
 
   def close
@@ -13,14 +14,18 @@ class PostgresqlPreparer
 
   protected
 
-  attr_reader :statement
+  attr_reader :name, :statement
 
   def sql
     "INSERT INTO people (height, weight, gender, created_at, updated_at)" +
     "  VALUES ($1, $2, $3, now(), now())"
   end
 
+  def connection
+    @connection ||= ActiveRecord::Base.connection.raw_connection
+  end
+
   def prepared_statement
-    ActiveRecord::Base.connection.raw_connection.prepare('insert_person', self.sql)
+    connection.prepare(name, sql)
   end
 end
