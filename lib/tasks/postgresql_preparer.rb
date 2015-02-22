ce036e5111732d7e10ffdb1ee25f7996e5d92756
William Montgomery <wmontg2@uic.edu>
2015-02-22 12:10:21 -0600

    Remove single quotes from postgres insert statement

--- lib/tasks/postgresql_preparer.rb
+++ lib/tasks/postgresql_preparer.rb
@@ -16,7 +16,7 @@ class PostgresqlPreparer
   attr_reader :statement
 
   def sql
-    "INSERT INTO people ('height', 'weight', 'gender', 'created_at', 'updated_at')" +
+    "INSERT INTO people (height, weight, gender, created_at, updated_at)" +
     "  VALUES ($1, $2, $3, now(), now())"
   end
 
